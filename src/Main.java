3c7c02dbd055b481e1cc6d9a1bd7c6cc904c55c3
Elaine Ulsh <elaine.ulsh@bsu.edu>
2025-01-27 12:22:50 -0500

    Add to Switch.java and Main.java.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.net.InetAddress;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        if (args.length != 2) {
+            System.out.println("Please specify switch 1 id: ");
+            return;
+        }
+        int s1ID = Integer.parseInt(args[0]);
+
+        Switch s1 = new Switch(s1ID);
     }
 }
--- src/Switch.java
+++ src/Switch.java
@@ -1,4 +1,15 @@
+import java.net.InetAddress;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Arrays;
 public class Switch {
     // TODO: get id as command-line argument, parses the config file to get its neighbors
     //
+    private int id;
+    private Parser parser = new Parser();
+
+    public Switch(int id) {
+        id = this.id;
+
+    }
 }
