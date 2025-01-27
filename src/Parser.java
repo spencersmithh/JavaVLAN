e97584bd4ea6659628ff90b8e7b513bd6ce08010
SLS-BSU <spencer.smith2@bsu.edu>
2025-01-27 15:34:28 -0500

    Edit config.properties and create getNeighbors()

--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,29 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class Parser {
-    //TODO: create parser!!
+    private final String name;
+    private final Properties properties = new Properties();
+
+    public Parser(String name) {
+        this.name = name;
+
+        try (FileInputStream fis = new FileInputStream("src/config.properties")) {
+            properties.load(fis);
+            String switch1ID=properties.getProperty("switch1");
+            System.out.println(switch1ID);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String[] getNeighbors() {
+        String rawConnections = properties.getProperty(name + "conn");
+        if (rawConnections == null) {
+            return new String[0];
+        }
+        rawConnections = rawConnections.replaceAll("[()\\s]", "");
+        return rawConnections.split(",");
+    }
 }
