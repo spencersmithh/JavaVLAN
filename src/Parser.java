fca79f05b8199088bd735d5820ec375963acdf6c
SLS-BSU <spencer.smith2@bsu.edu>
2025-01-27 15:47:13 -0500

    getID() and config.properties changes

--- src/Parser.java
+++ src/Parser.java
@@ -18,12 +18,26 @@ public class Parser {
             e.printStackTrace();
         }
     }
+
+    public String[] getID() {
+        String rawID = properties.getProperty(name);
+        if (rawID == null) {
+            throw new IllegalArgumentException("No entry found for " + name + " in config.properties");
+        }
+        rawID = rawID.replaceAll("()", "");
+        String[] id = rawID.split(",");
+        if (id.length != 2) {
+            throw new IllegalArgumentException("Invalid format for entry: " + name + " in config.properties");
+        }
+        return id;
+    }
+
     public String[] getNeighbors() {
         String rawConnections = properties.getProperty(name + "conn");
         if (rawConnections == null) {
-            return new String[0];
+            throw new IllegalArgumentException("No connection entries found for " + name + " in config.properties");
         }
-        rawConnections = rawConnections.replaceAll("[()\\s]", "");
+        rawConnections = rawConnections.replaceAll("()", "");
         return rawConnections.split(",");
     }
 }
