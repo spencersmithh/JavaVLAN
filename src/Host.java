607597c2c2c1e82532988e596a063355fe7aa852
Jacob Clark <clarkjac03@gmail.com>
2025-01-29 10:33:12 -0500

    Implement Base Methods to Host

--- src/Host.java
+++ src/Host.java
@@ -1,3 +1,42 @@
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class Host {
-    //TODO: host stuff
+    String name = "";
+    int port = 0;
+    String ip = "";
+
+    //String id = parser.getID(name);
+    //port = split from id
+    //ip = split from id
+
+    //String id = ip + ":" + port;
+
+    String frameMessage;
+    public Host(String id){
+
+    }
+
+    private byte[] convertStringToBytes(String string){
+        Charset UTF_8 = StandardCharsets.UTF_8;
+        return string.getBytes(UTF_8);
+    }
+    private String convertBytesToBinary(byte[] bytes){
+        StringBuilder binary = new StringBuilder();
+        for (byte b : bytes){
+            int val = b;
+            for(int i=0;i<8;i++){
+                binary.append((val&128)==0?0:1);
+                val<<=1;
+            }
+            binary.append(' ');
+        }
+        return binary.toString();
+    }
+    private String encryptBinary(String OGBinary){
+        return null;
+    }
+    private byte[] convertBinaryToBytes(String binary){
+        return null;
+    }
 }
