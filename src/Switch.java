36f079287c3e1c567834853cf6e32ea16d6a6956
Elaine Ulsh <elaine.ulsh@bsu.edu>
2025-01-28 23:16:07 -0500

    Adjust the Switch.java file to include a main method and some UDP practices.

--- src/Switch.java
+++ src/Switch.java
@@ -4,26 +4,39 @@ import java.net.DatagramSocket;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 public class Switch {
-    // TODO: get id as command-line argument, parses the config file to get its neighbors
-    private String name;
-    private InetAddress IP;
-    private int portNumber;
-    private final Parser parser;
-    private String[] neighbors;
+    public static void main(String[] args) throws Exception {
+        String switchID = args[0];
+        Parser parser = new Parser(switchID);
 
-    public Switch(String name) {
-        name = this.name;
-        parser = new Parser(name);
+        // TBH I'm not exactly sure what I wanted this for...
         try {
-            IP = InetAddress.getByName(parser.getID()[0]);
+            InetAddress IP = InetAddress.getByName(parser.getID()[0]);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-        portNumber = Integer.parseInt(parser.getID()[1]);
-        neighbors = parser.getNeighbors();
-    }
+        int portNumber = Integer.parseInt(parser.getID()[1]);
 
-    // TODO: main
+        String[] neighbors = parser.getNeighbors();
+
+        // create the switch table for port and hosts
+
+        DatagramSocket socket = new DatagramSocket(portNumber);
+        DatagramPacket frameRequest = new DatagramPacket(new byte[1024], 1024);
+
+        while (true) {
+            socket.receive(frameRequest);
+            byte[] frame = Arrays.copyOf(frameRequest.getData(), frameRequest.getLength());
+            // get the srcMAC and destMAC by deciphering the frame
+
+            // update the table with the srcMAC if it's not already in there
+
+            // check to see if destMAC is in the table
+
+            // if it is, send directly to the destMAC
+
+            // else, flood the LAN and adjust the table
+        }
+    }
 
     // TODO: receive frame, decipher the frame, srcMAC, destMAC, search table, flood, adjust table
 
