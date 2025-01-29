a3c63e43da3788769c45147f72e578efccbb5621
Jacob Clark <clarkjac03@gmail.com>
2025-01-29 12:42:28 -0500

    Remove Unnecessary Functions

--- src/Host.java
+++ src/Host.java
@@ -1,48 +1,41 @@
-import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class Host {
-    String name = "";
+    String name = "hostA";
 
     Parser parser = new Parser(name);
-    String[] id = parser.getID();
-    String port = id[0];
-    InetAddress ip = InetAddress.getByName(parser.getID()[0]);
+    //String[] id = parser.getID();
+    //String port = id[1];
+    //InetAddress ip = InetAddress.getByName(id[0]);
 
     String frameMessage;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+        String testString = "Hello";
+        Host host = new Host("hostA");
+        
+        //This code block just tests those two functions
+        /*
+        byte[] bytes = host.convertStringToBytes(testString);
+        System.out.println(bytes);
+        String outString = host.convertBytestoString(bytes);
+        System.out.println(outString);
+         */
+    }
+
+    public Host(String name) throws UnknownHostException {
 
     }
 
-    public Host(String id) throws UnknownHostException {
-
-    }
-
-    private byte[] convertStringToBytes(String string){
+    public byte[] convertStringToBytes(String string){
         Charset UTF_8 = StandardCharsets.UTF_8;
         return string.getBytes(UTF_8);
     }
-    private String convertBytesToBinary(byte[] bytes){
-        StringBuilder binary = new StringBuilder();
-        for (byte b : bytes){
-            int val = b;
-            for(int i=0;i<8;i++){
-                binary.append((val&128)==0?0:1);
-                val<<=1;
-            }
-            binary.append(' ');
-        }
-        return binary.toString();
-    }
-    private String encryptBinary(String OGBinary){
-        return null;
-    }
-    private byte[] convertBinaryToBytes(String binary){
-        byte[] newBytes = new BigInteger(binary, 2).toByteArray();
-        return newBytes;
+    private String convertBytestoString(byte[] bytes){
+        String s = new String(bytes, StandardCharsets.UTF_8);
+        return s;
     }
 }
