b1921092fb61c972cef7cd91312bc0f8c3bf85c7
Lukas Maynard <104531583+Lukas-Maynard@users.noreply.github.com>
2025-02-04 18:54:38 -0500

    yuh

--- src/Host.java
+++ src/Host.java
@@ -4,14 +4,16 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Host{
-    private String name;
-    private String id;
-    private int port;
-    private InetAddress ip;
-    private String[] mac;
-    static String[] neighbors;
+    private static String name;
+    private static String id;
+    private static int port;
+    private static InetAddress ip;
+    private static String[] mac;
+    private static String[] neighbors;
     private static DatagramSocket socket;
     private volatile boolean running = true;
 
@@ -36,88 +38,71 @@ public class Host{
                         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                         socket.receive(packet);
 
-                        String inputFrame = new String(packet.getData(),0, packet.getLength());
+                        String inputFrame = new String(packet.getData(), 0, packet.getLength());
 
                         // Parse frame
                         String[] frameParts = inputFrame.split(";");
-                        if (frameParts.length < 3){
+                        if (frameParts.length < 3) {
                             System.out.println("Frame has incorrect length");
                             continue;
                         }
 
-                        Parser incommingMAC = new Parser(frameParts[0]);
-                        Parser ourMAC = new Parser(frameParts[1]);
-
-                        // TODO: test if the destination MAC matches the current host MAC.
-                        //      if it does, print out the message.
-                        //      if it doesn't, ignore.
-
-                        // somehow figure out where to put the sock.close() statement where it's not unreachable.
-                        if (dest.getMAC() == src.getMAC()) {
-                            src.processPacket(reply);
-                        }
-
-                        processPacket(packet);
+                        System.out.println("Incoming Packet: ");
+                        System.out.println(Arrays.toString(frameParts));
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 }
             }
+        }
 
-    private static void processPacket(DatagramPacket packet) {
-        System.out.println("Received packet from: " + packet.getAddress().getHostAddress());
-        String data = new String(packet.getData(), 0, packet.getLength());
-        System.out.println("Data: " + data);
-    }
+        static class send implements Runnable {
+            @Override
+            public void run() {
+                String[] destinationMac = new String[2];
+                String message = "";
 
-    public void stop() {
-        running = false;
-        socket.close();
-    }
+                while (true) {
+                    Scanner keyInput = new Scanner(System.in);
+                    System.out.println("Enter the destMAC (IP and port number) and message separated by a space");
+                    String userRequest = keyInput.nextLine();
 
-    public static void main(String[] args) throws Exception {
+                    destinationMac[0] = userRequest.split(" ",3)[0];
+                    destinationMac[1] = userRequest.split(" ",3)[1];
+                    message = userRequest.split(" ",3)[2];
+                    String frameMessage = Arrays.toString(mac) +";"+ destinationMac[0] + "," + destinationMac[1] + ";" + message;
+
+                    byte[] frameBytes = convertStringToBytes(frameMessage);
+                    Parser neighbor = getNeighborParser();
+
+                    // START OF UDP IMPLEMENTATION
+
+                    DatagramPacket request = null;
+                    try {
+                        request = new DatagramPacket(frameBytes, frameBytes.length, neighbor.getIP(), neighbor.getPort());
+                        socket.send(request);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("The host named " + name + " has send a message to a device with the following MAC address: " + Arrays.toString(destinationMac));
+                }
+            }
+        }
+
+    public static void main(String[] args) {
         // START OF USER INPUT AND DEFINING VARIABLES
-        String[] destinationMac = new String[2];
-        String message = "";
 
         if (args[0].isEmpty()){
             System.out.println("hostname/arguments not given in run configuration...");
             System.exit(1);
         }
-        String hostname = args[0];
-        Host host = new Host(hostname);
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        Runnable threadListen = new listen();
+        Runnable threadSend = new send();
+        es.submit(threadListen);
+        es.submit(threadSend);
 
-        Scanner keyInput = new Scanner(System.in);
-        System.out.println("Enter the destMAC (IP and port number) and message separated by a space");
-        String userRequest = keyInput.nextLine();
-
-        destinationMac[0] = userRequest.split(" ",3)[0];
-        destinationMac[1] = userRequest.split(" ",3)[1];
-        message = userRequest.split(" ",3)[2];
-        String frameMessage = Arrays.toString(host.mac) +";"+ destinationMac[0] + "," + destinationMac[1] + ";" + message;
-
-        byte[] frameBytes = host.convertStringToBytes(frameMessage);
-        Parser neighbor = getNeighborParser();
-
-        // START OF UDP IMPLEMENTATION
-        try {
-            DatagramSocket socket = host.getSocket();
-            Thread thread = new Thread(host);
-            thread.start();
-
-            DatagramPacket request = new DatagramPacket(frameBytes, frameBytes.length, neighbor.getIP(), neighbor.getPort());
-            socket.send(request);
-
-            System.out.println("The host named " + hostname + " has send a message to a device with the following MAC address: " + Arrays.toString(destinationMac));
-
-            while (host.running) { // listening...
-                DatagramPacket reply = new DatagramPacket(new byte[1024], 1024);
-                socket.receive(reply);
-            }
-
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
     }
 
     public Object[] getMac() {
@@ -129,13 +114,17 @@ public class Host{
     private static Parser getNeighborParser() {
         return new Parser(neighbors[0]);
     }
-    private byte[] convertStringToBytes(String string){
+    private static byte[] convertStringToBytes(String string){
         Charset UTF_8 = StandardCharsets.UTF_8;
         return string.getBytes(UTF_8);
     }
 
-    private String convertBytesToString(byte[] bytes){
+    private static String convertBytesToString(byte[] bytes){
         String s = new String(bytes, StandardCharsets.UTF_8);
         return s;
     }
 }
+
+// TODO: test if the destination MAC matches the current host MAC.
+//      if it does, print out the message.
+//      if it doesn't, ignore.
